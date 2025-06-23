ede031b7d9b6c4cc72161f32971c52b5bb875615
Jronin <Jronin22@gmail.com>
2025-06-22 23:44:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-[]
+from fastapi import FastAPI
+from dice import roll_1d20, roll_2d20
+from npc import generate_npc
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"welcome": "Dune TTRPG GM Tools - API is live."}
+
+@app.get("/roll/1d20")
+def api_roll_1d20():
+    return roll_1d20()
+
+@app.get("/roll/2d20")
+def api_roll_2d20():
+    return roll_2d20()
+
+@app.get("/npc")
+def api_generate_npc():
+    return generate_npc()
